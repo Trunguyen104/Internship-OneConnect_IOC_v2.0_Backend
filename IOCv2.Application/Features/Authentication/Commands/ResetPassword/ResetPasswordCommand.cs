f96ac3fb8d51b45224f2dd4542659a6d60bfaa89
nnguyenbon <nguyenbaongyn@gmail.com>
2026-02-09 00:45:14 +0700

    feat: update login resetpassword

---
+++ IOCv2.Application/Features/Authentication/Commands/ResetPassword/ResetPasswordCommand.cs
@@ -0,0 +1,12 @@
+﻿using IOCv2.Application.Common.Models;
+using MediatR;
+
+namespace IOCv2.Application.Features.Authentication.Commands.ResetPassword
+{
+    public record ResetPasswordCommand
+    (
+        string Token,
+        string NewPassword,
+        string ConfirmPassword
+    ) : IRequest<Result<string>>;
+}
