9844d8f1687d1a946f4586a509f77d1ab4274a2a
Nguyen Trung Nguyen <nguyennt.ce191135@gmail.com>
2026-03-04 09:22:53 +0700

    feat: Add ProjectResourcesDTO for project resources in GetProjectById query.

---
+++ IOCv2.Application/Features/Projects/Queries/GetProjectById/ProjectResourcesDTO.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using IOCv2.Application.Extensions.Mappings;
+using IOCv2.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IOCv2.Application.Features.Projects.Queries.GetProjectById
+{
+    public class ProjectResourcesDTO : IMapFrom<Domain.Entities.ProjectResources>
+    {
+        public Guid ProjectId { get; set; }
+        public string ResourceName { get; set; } = string.Empty;
+        public string ResourceType { get; set; } = string.Empty;
+        public string ResourceUrl { get; set; } = string.Empty;
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<Domain.Entities.ProjectResources, ProjectResourcesDTO>()
+                .ForMember(dest => dest.ResourceType, opt => opt.MapFrom(src => src.ResourceType.ToString()));
+        }
+    }
+}
