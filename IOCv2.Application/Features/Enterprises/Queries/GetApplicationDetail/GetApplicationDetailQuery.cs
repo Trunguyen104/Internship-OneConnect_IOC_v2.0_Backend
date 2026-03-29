97a742898e1b00ea331397adad1a741362cafe3c
MinhTCCE190895 <minhtc.ce190895@gmail.com>
2026-03-29 16:47:00 +0700

    refactor: remove redundant GetApplicationDetail and GetEnterpriseApplications from Enterprises (replaced by HRApplications feature)

--- IOCv2.Application/Features/Enterprises/Queries/GetApplicationDetail/GetApplicationDetailQuery.cs
+++
@@ -1,9 +0,0 @@
-using IOCv2.Application.Common.Models;
-using MediatR;
-
-namespace IOCv2.Application.Features.Enterprises.Queries.GetApplicationDetail;
-
-public record GetApplicationDetailQuery : IRequest<Result<GetApplicationDetailResponse>>
-{
-    public Guid ApplicationId { get; init; }
-}
