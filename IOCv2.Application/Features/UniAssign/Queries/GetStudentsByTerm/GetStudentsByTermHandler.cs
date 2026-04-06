2498603509028ad4356d0dbc4b1adb73730721c3
thuanphatce191406-ai <thuanphat.ce191406@gmail.com>
2026-04-06 17:20:53 +0700

    fix: get phase status

--- IOCv2.Application/Features/UniAssign/Queries/GetStudentsByTerm/GetStudentsByTermHandler.cs
+++ IOCv2.Application/Features/UniAssign/Queries/GetStudentsByTerm/GetStudentsByTermHandler.cs
@@ -87,17 +87,8 @@ namespace IOCv2.Application.Features.UniAssign.Queries.GetStudentsByTerm
                     StudentName = st.Student.User.FullName,
                     ClassName = st.Student.ClassName ?? string.Empty,
                     Major = st.Student.Major ?? string.Empty,
-                    PlacementStatus = _unitOfWork.Repository<StudentTerm>()
-                        .Query()
-                        .Where(a => a.TermId == request.TermId && a.StudentId == st.StudentId)
-                        .Select(a => a.PlacementStatus)
-                        .FirstOrDefault(),
-                    EnterpriseName = _unitOfWork.Repository<StudentTerm>()
-                        .Query()
-                        .Include(x => x.Enterprise)
-                        .Where(a => a.TermId == request.TermId && a.StudentId == st.StudentId)
-                        .Select(a => a.Enterprise!.Name)
-                        .FirstOrDefault(),
+                    PlacementStatus = st.PlacementStatus,
+                    EnterpriseName = st.Enterprise!.Name,
                     InternPhaseName = _unitOfWork.Repository<InternshipPhase>().Query()
                         .Where(p => p.PhaseId == _unitOfWork.Repository<InternshipApplication>().Query().Where(ia => ia.TermId == request.TermId && ia.StudentId == st.StudentId).Select(ia => ia.InternPhaseId).FirstOrDefault())
                         .Select(p => p.Name)
